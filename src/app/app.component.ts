fafadb14fd6451e291ccb04666dc02aec025d6e1
Armiron Kurbalaj <armironkurbalaj@gmail.com>
2022-08-16 15:58:15 +0200

    Added card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'Saw this awesome tree through my hike today',
+    },
+    {
+      title: 'Snowy mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountain',
+      content: 'Saw this awesome mountain through my hike today',
+    },
+    {
+      title: 'Mountain biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking',
+      content: 'I did some biking today',
+    },
+  ];
 }
